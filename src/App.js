ae26903af5a50a7dde9301910626e8f974335639
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-06-21 21:05:20 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import { useEffect, useState } from "react";
-// import Player from "./components/Player";
 import axios from "axios";
-// import Pagination from "./components/Pagination";
-import PlayerControl from "./components/PlayerControl";
+import { useState, useEffect } from "react";
+import Player from "./components/Player/Player";
 
 function App() {
-
-
-  
+  const [songs, setSongs] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+  useEffect(() => {
+    async function getData() {
+      const request = await axios.get(
+        "https://free-audios-api.herokuapp.com/audio"
+      );
+      setSongs(request.data);
+      setLoaded(true);
+    }
+    getData();
+  }, []);
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
 
   return (
-    <div className="app">
-      <PlayerControl/>
-
+    <div className="App">
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+        loaded={loaded}
+      />
     </div>
   );
 }
